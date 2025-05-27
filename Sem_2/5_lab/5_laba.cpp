545c9c24c98b6bc40a7e77909712b0ee8a7193bc
nEr <sifsner@gmail.com>
2025-05-27 10:36:38 +0500

    Update 5_laba.cpp

--- Sem_2/5_lab/5_laba.cpp
+++ Sem_2/5_lab/5_laba.cpp
@@ -1,24 +1,39 @@
 #include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
-void func(int a)
-{
-    for (int i = 0; i < 3; i++ )
-    {   
-        cout << "(";
-        for (int j = 0; j < 10-1; j++)
-        {
-            if (a[i][j] == 0)
-            {
-                cout << ",";
+
+void printWords(const string& line) {
+    cout << "(";
+    string word;
+    bool first = true;
+
+    for (char ch : line) {
+        if (ch != '0') {
+            word += ch;
+        } else {
+            if (!word.empty()) {
+                if (!first) cout << ",";
+                cout << word;
+                word.clear();
+                first = false;
             }
-            else cout << a[i][j];
         }
-        cout << ")";
     }
+
+    cout << ")";
 }
-int main()
-{
-    int a[3][10] = {{1,2,3,0,4,5,6,0,9,0}, {1,2,3,0,4,5,6,0,9,0}, {1,2,3,0,4,5,6,0,9,0}};
-    
+
+int main() {
+    vector<string> lines = {
+        "12304506090",
+        "2344502340",
+        "2344555670"
+    };
+
+    for (const string& line : lines)
+        printWords(line);
+
+    cout << endl;
     return 0;
 }
